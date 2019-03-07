36ee7f7c2794cfd0b697cd3f91eee3688eaca2d1
Connor Onweller <onweller@udel.edu>
2019-03-07 16:11:24 -0500

    more tree stuff

--- Tree.java
+++ Tree.java
@@ -1,14 +1,67 @@
 import java.util.*;
 
-enum Label{
-	YES, NO, PARENT
-}
+//TODO:
+//	* Impliment graph
+//	* Use a placeholder function that just chooses a random attribute
 
-class Node{
-	String name;
-	int index;
-	ArrayList<Integer> subset;
-	Node left;
-	Node right;
-	Label label;
+public class Tree {
+	private Node root;
+	ArrayList<ArrayList<Integer>> trainingData;
+	String[] trainingDataNames;
+
+	public class Node {
+		int index;	// location in super array
+		ArrayList<Integer> subset; //array of acceptable index
+		Node zero;	//left
+		Node one;	//right
+
+		public Node(int index, ArrayList<Integer> subset) {
+			this.index = index;
+			this.subset = subset;
+		}
+
+
+		@Override
+		public String toString(){
+			return trainingDataNames[index];
+		}
+	}
+
+	public Tree(ArrayList<ArrayList<Integer>> trainingData, String[] trainingDataNames) {
+		this.trainingData = trainingData;
+		this.trainingDataNames = trainingDataNames;
+
+	}
+
+	public void buildTree() {
+		int rootIndex = chooseRandomAttribute();
+		ArrayList<Integer> set = new ArrayList<>();
+		System.out.println(trainingData);
+		for(int i = 0; i < trainingData.size(); i++) {
+			set.add(i);
+		}
+
+		root = new Node(rootIndex, set);
+		System.out.println(root);
+		
+		
+	}
+
+
+	// Returns the subset of usable data at a Node
+	ArrayList<Integer> get_subset(Node parent, int val) {
+		ArrayList<Integer> newSubset = new ArrayList<>();
+		for (Integer ki: parent.subset)
+			if(trainingData.get(parent.index).get(ki)==val)
+				newSubset.add(ki);
+		return newSubset;
+	}
+
+	int chooseRandomAttribute(){
+		return 0;
+	}
+	boolean isPure(){
+		return true;
+	}
+
 }
