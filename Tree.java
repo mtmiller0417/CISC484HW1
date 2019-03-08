716deb6b234814fb156c490a2afa281b85033898
Connor Onweller <onweller@udel.edu>
2019-03-07 20:29:54 -0500

    added a few comments

--- Tree.java
+++ Tree.java
@@ -1,80 +1,73 @@
 import java.util.*;
 
-//TODO:
-//	* Impliment graph
-//	* Use a placeholder function that just chooses a random attribute
 
 
-// nodes are basically attributes
+// Stuff About the Tree:
+// Each node of the tree is an attribute
 
 public class Tree {
-	private Node root;
+	private AttributeNode root;
 	ArrayList<ArrayList<Integer>> trainingData;
 	String[] trainingDataNames;
 
-	public class Node {
+	public class AttributeNode {
 		int index;	// location of attribute in super array
-		ArrayList<Integer> subset; //array of acceptable index
-		Node zero;	//left
-		Node one;	//right
-		public Node(int index, ArrayList<Integer> subset) {
+		ArrayList<Integer> subset; //array of acceptable index (values)
+		AttributeNode zero;	//left (Attribute=0)
+		AttributeNode one;	//right (Attribute=1)
+		public AttributeNode(int index, ArrayList<Integer> subset) {
 			this.index = index;
 			this.subset = subset;
 		}
-		// public Node leafNode(){
-		// 	return 0;
-		// }
-
 
 		@Override
 		public String toString(){
 			return ((Integer)index).toString();
 		}
 	}
-
+	
 	public Tree(ArrayList<ArrayList<Integer>> data, String[] names) {
 		this.trainingData = data;
 		this.trainingDataNames = names;
-
 	}
 
-	public void buildTree() {
+	
+	// Main Method
+	public void buildDecisonTree() {
 		ArrayList<Integer> set = new ArrayList<>();
-		// System.out.println(trainingData);
 		for(int i = 0; i < trainingData.get(0).size(); i++) {
 			set.add(i);
 		}
 		int rootIndex = chooseRandomAttribute(set);
-		root = new Node(rootIndex, set);
-		expandNode(root, set);
+		root = new AttributeNode(rootIndex, set);
+		expandNode(root);
 	}
 
-
-	public void expandNode(Node node, ArrayList<Integer> subset){
-		System.out.println(node.index);
+		
+	public void expandNode(AttributeNode node){
 		ArrayList<Integer> subset0 = getSubset(node, 0);
 		ArrayList<Integer> subset1 = getSubset(node, 1);
 		if (isPure(subset0)) {
 			node.zero = null;
 		} else {
 			int index = chooseRandomAttribute(subset0);
-			node.zero = new Node(index, subset0);
-			expandNode(node.zero, subset0);
+			node.zero = new AttributeNode(index, subset0);
+			expandNode(node.zero);
 		}
 
 		if (isPure(subset1)) {
 			node.one = null;
 		} else {
 			int index = chooseRandomAttribute(subset1);
-			node.one = new Node(index, subset1);
-			expandNode(node.one, subset1);
+			node.one = new AttributeNode(index, subset1);
+			expandNode(node.one);
 		}
 		
 	}
 
 	// Returns the subset of usable data at an attribue node
-	// given an attribute value
-	ArrayList<Integer> getSubset(Node parent, int val) {
+	// given an attribute value for that node
+	ArrayList<Integer> getSubset(AttributeNode parent, int val) {
 		ArrayList<Integer> newSubset = new ArrayList<>();
 		for (Integer i: parent.subset)
 			if(trainingData.get(parent.index).get(i)==val)
@@ -87,9 +80,11 @@ public class Tree {
 		return random.nextInt(trainingData.size());
 	}
 
+	// true if subset is pure
 	boolean isPure(ArrayList<Integer> subset){
-		if (subset.isEmpty())
-			return true;
+		if (subset.isEmpty()){ 	// TODO: not entirely sure what we do when
+			return true;		// a subset is empty
+		}
 		int firstVal =
 			trainingData.get(trainingData.size()-1).get(subset.get(0));
 		for (Integer i: subset)
